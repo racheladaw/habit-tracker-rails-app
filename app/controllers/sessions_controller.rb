a6c3199c03d2ab494edc97f16f2aa6eb76a06948
Rachel <williamsrachelada@gmail.com>
2019-07-17 15:22:54 -0700

    Added basic authentication, login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to new_session_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:new, :create, :show]
+
+  resources :sessions, only: [:new, :create]
+
+  post '/logout' => 'sessions#destroy'
 end
