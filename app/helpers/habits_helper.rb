cccaec123f24e5fe9f55f80a89071ae204df3b31
Rachel <williamsrachelada@gmail.com>
2019-08-02 10:48:55 -0700

    Created two helper methods in habit helper to check if a category has habits and if a category has popular habits

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -1,2 +1,11 @@
 module HabitsHelper
+
+  def category_has_popular_habits?(category)
+    !category.popular_habits_in_category.empty?
+  end
+
+  def category_has_habits?(category)
+    !category.habits.empty?
+  end
+
 end
