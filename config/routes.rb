ca422012330fe52156ce278ef243f02d2e3f67d2
Adrien <github@adrien.sent.com>
2019-10-31 13:30:54 +0100

    style(routes): ordered/symbolized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,27 @@
 Rails.application.routes.draw do
   root to: 'users#home'
 
-  resources :users, only: [:new, :create, :show]
+  resources :categories, only: [:new, :create, :index] do
+    resources :habits, only: [:index, :new]
+  end
 
-  resources :sessions, only: [:new, :create]
+  resources :goals, only: [:create, :index, :update, :destroy]
 
   resources :habits, only: [:create] do
     resources :goals, only: [:new, :show, :edit]
   end
 
-  resources :goals, only: [:create, :index, :update, :destroy]
+  resources :sessions, only: [:new, :create]
 
-  resources :categories, only: [:new, :create, :index] do
-    resources :habits, only: [:index, :new]
-  end
+  resources :users, only: [:new, :create, :show]
 
-  post '/logout' => 'sessions#destroy'
+  post :logout, to: 'sessions#destroy'
 
-  get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/facebook/callback', to: 'sessions#create'
 
   get :home, to: 'users#home'
 
-  post '/completed_habit/:id' => 'goals#completed_habit', as: 'completed_habit'
+  post '/completed_habit/:id', to: 'goals#completed_habit', as: 'completed_habit'
 
-  get '/greatest_user' => 'users#greatest_user'
-
-
+  get '/greatest_user', to: 'users#greatest_user'
 end
